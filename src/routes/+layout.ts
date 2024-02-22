702e7d8a9b7a844f624a1e0a0c1bf8965317d532
Harishankar G <harishankargaddanakeri@gmail.com>
2024-02-22 18:16:43 +0530

    Use vercel analytics and speed insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,7 @@
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+injectSpeedInsights();
+inject({ mode: dev ? 'development' : 'production' });
+
 export const prerender = true;
